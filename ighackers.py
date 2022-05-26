cef239dc6ed6ada892d85923bf388351e098cd24
coloramamara <106280766+cololoramara@users.noreply.github.com>
2022-05-26 23:21:18 +0200

    IG hackers

---
+++ ighackers.py
@@ -0,0 +1,26 @@
+  
+print(" _____  _____  _   _  _____   ____  _   _ ")  
+print("|_   _|| ____|| | | ||  _  | /  __|| | | |")
+print("  | |  ||  __ | | | || | | ||  /   | |/ / ")
+print("  | |  || |_ || |_| || |_| || |    |   /")
+print("  | |  ||   |||  _  ||  _  || |    |   \ ") 
+print(" _| |_ ||___||| | | || | | ||  \__ | |\ \ ") 
+print("|_____||_____||_| |_||_| |_| \____||_| |_| ")
+print("made by skylem or cololoramara")
+
+input("who want to hack: ")
+
+input("click enter get password")
+
+import random
+import webbrowser
+
+names = ['**71rmop@', 'solo***********pt05n', 'rgrgeg*413545gse', 'icvvbdv5', 'ifdnh*214', 'tirjuitrgt-']
+def selectRandom(names):
+  return random.choice(names)
+
+print("the password: ", selectRandom(names))
+
+input ("press ENTER to get to get redirected to ig")
+
+webbrowser.open ("instagram.com")
